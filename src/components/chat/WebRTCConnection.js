e3e6ae3fa9510a10914286885c0dfcda668acec2
Joel Sequeira <joelseq96@gmail.com>
2018-07-21 18:52:36 -0700

    Change TURN servers

--- src/components/chat/WebRTCConnection.js
+++ src/components/chat/WebRTCConnection.js
@@ -10,17 +10,17 @@ const config = {
   iceServers: [
     { urls: 'stun:stun.services.mozilla.com' },
     { urls: 'stun:stun.l.google.com:19302' },
+    {
+      urls: 'turn:13.250.13.83:3478?transport=udp',
+      credential: 'YzYNCouZM1mhqhmseWk6',
+      username: 'YzYNCouZM1mhqhmseWk6',
+    },
+    {
+      urls: 'turn:13.250.13.83:3478?transport=tcp',
+      credential: 'YzYNCouZM1mhqhmseWk6',
+      username: 'YzYNCouZM1mhqhmseWk6',
+    },
     { urls: 'turn:numb.viagenie.ca', credential: 'muazkh', username: 'webrtc@live.com' },
-    {
-      urls: 'turn:192.158.29.39:3478?transport=udp',
-      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      username: '28224511:1379330808',
-    },
-    {
-      urls: 'turn:192.158.29.39:3478?transport=tcp',
-      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      username: '28224511:1379330808',
-    },
   ],
 }
 
